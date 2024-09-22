2eecdf7f81e400bd41bd072851d950efc83547f6
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-09-22 13:01:55 +0530

    Update ProductControl.js

--- controllers/ProductControl.js
+++ controllers/ProductControl.js
@@ -211,10 +211,17 @@ exports.deleteReview = catchAsyncError(async (req, res, next) =>{
 });
 
 // get admin products  - api/v1/admin/products
-exports.getAdminProducts = catchAsyncError(async (req, res, next) =>{
+exports.getAdminProducts = catchAsyncError(async (req, res, next) => {
     const products = await Product.find();
-    res.status(200).send({
+    
+    if (products.length === 0) {
+        return res.status(404).json({
+            success: false,
+            message: "No products found."
+        });
+    }
+    res.status(200).json({
         success: true,
         products
-    })
+    });
 });
