dfe03e8898e5895b24e06b0190fbeaa6aa1c698e
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-09-25 14:16:44 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,31 +3,24 @@ const User = require('../models/userModel')
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require('jsonwebtoken');
 exports.isAuthenticatedUser =  catchAsyncError(async (req, res, next) => {
- const ticket = req.header("Authorization")?.split ("") [1];
- if(!ticket)
-    return res.status(401).json({message:"Access Denied"});
-try{
-    const  verified =jwt.verify(ticket,process.env.JWT_SECRET);
-    req.userId = verified.user.id;
-    next();
-} catch(error){
-    return res.status(401).json({message:"Token is not valid"});
- 
-}
-})
+const { ticket } = req.cookies;
+
+ if(!ticket){
+    return next(new ErrorHandler('login first to access this route', 401));
+   
+ }
+
+ const decoded = jwt.verify(ticket, process.env.JWT_SECRET)
+ req.user = await User.findById(decoded.id)
+ next();
+
+
+   })
    exports.authorizeRoles =(...roles) => {
-    return  async (req, res, next) => {
-        try{
-            const user = await User.findById(req.userId);
-            if(!user || !roles.includes(req.user.role)){
-                return res.status(403).json({message:"Token is not valid"});
-            }
-           
-           next();
-        }catch(error){
-            res.status(500).json({message:"Server error"})
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed`,401));
         }
-        
-      
-    };
-   };
+        next()
+    }
+   }
